012ada42c6711e92f83adb560ef9c022dcd2f494
embrown18 <35782514+embrown18@users.noreply.github.com>
2020-03-04 19:01:23 -0600

    updates for a second call to geojson

---
+++ Lab4/Covid_Rate.js
@@ -0,0 +1,652 @@
+var dataC =
+{
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 112.23,30.73 ]
+    },
+    "properties": {
+    "FID":1,
+    "Providence":"Hubei",
+    "1":7153,
+    "2":9074,
+    "3":11177,
+    "4":13522,
+    "5":16678,
+    "6":19665,
+    "7":22112,
+    "8":24953
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 119.78,29.14 ]
+    },
+    "properties": {
+    "FID":2,
+    "Providence":"Zhejiang",
+    "1":599,
+    "2":661,
+    "3":742,
+    "4":829,
+    "5":895,
+    "6":954,
+    "7":1006,
+    "8":1048
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 113.76,23.37 ]
+    },
+    "properties": {
+    "FID":3,
+    "Providence":"Guangdong",
+    "1":250,
+    "2":604,
+    "3":683,
+    "4":797,
+    "5":870,
+    "6":944,
+    "7":1018,
+    "8":1075
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 113.38,34.29 ]
+    },
+    "properties": {
+    "FID":4,
+    "Providence":"Henan",
+    "1":422,
+    "2":493,
+    "3":566,
+    "4":675,
+    "5":764,
+    "6":851,
+    "7":914,
+    "8":981
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 111.85,27.62 ]
+    },
+    "properties": {
+    "FID":5,
+    "Providence":"Hunan",
+    "1":389,
+    "2":463,
+    "3":521,
+    "4":593,
+    "5":661,
+    "6":711,
+    "7":772,
+    "8":803
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 117.92,30.6 ]
+    },
+    "properties": {
+    "FID":6,
+    "Providence":"Anhui",
+    "1":297,
+    "2":340,
+    "3":408,
+    "4":480,
+    "5":530,
+    "6":591,
+    "7":665,
+    "8":733
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 114.9,27.08 ]
+    },
+    "properties": {
+    "FID":7,
+    "Providence":"Jiangxi",
+    "1":286,
+    "2":333,
+    "3":391,
+    "4":476,
+    "5":548,
+    "6":600,
+    "7":661,
+    "8":698
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 106.91,29.43 ]
+    },
+    "properties": {
+    "FID":8,
+    "Providence":"Chongqing",
+    "1":238,
+    "2":262,
+    "3":300,
+    "4":337,
+    "5":366,
+    "6":389,
+    "7":411,
+    "8":426
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 102.8,30.26 ]
+    },
+    "properties": {
+    "FID":9,
+    "Providence":"Sichuan",
+    "1":207,
+    "2":236,
+    "3":254,
+    "4":282,
+    "5":301,
+    "6":321,
+    "7":344,
+    "8":363
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 119.78,33.14 ]
+    },
+    "properties": {
+    "FID":10,
+    "Providence":"Jiangsu",
+    "1":202,
+    "2":231,
+    "3":271,
+    "4":308,
+    "5":341,
+    "6":373,
+    "7":408,
+    "8":439
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 117.92,35.89 ]
+    },
+    "properties": {
+    "FID":11,
+    "Providence":"Shandong",
+    "1":202,
+    "2":225,
+    "3":246,
+    "4":270,
+    "5":298,
+    "6":343,
+    "7":379,
+    "8":407
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 116.4,39.9 ]
+    },
+    "properties": {
+    "FID":12,
+    "Providence":"Beijing",
+    "1":156,
+    "2":183,
+    "3":212,
+    "4":228,
+    "5":253,
+    "6":274,
+    "7":297,
+    "8":315
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 121.47,31.23 ]
+    },
+    "properties": {
+    "FID":13,
+    "Providence":"Shanghai",
+    "1":153,
+    "2":177,
+    "3":193,
+    "4":208,
+    "5":233,
+    "6":254,
+    "7":269,
+    "8":281
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 117.92,26.48 ]
+    },
+    "properties": {
+    "FID":14,
+    "Providence":"Fujian",
+    "1":144,
+    "2":159,
+    "3":179,
+    "4":194,
+    "5":205,
+    "6":215,
+    "7":224,
+    "8":239
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 109.18,35.39 ]
+    },
+    "properties": {
+    "FID":15,
+    "Providence":"Shaanxi",
+    "1":101,
+    "2":116,
+    "3":128,
+    "4":142,
+    "5":165,
+    "6":173,
+    "7":184,
+    "8":195
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 108.8,23.72 ]
+    },
+    "properties": {
+    "FID":16,
+    "Providence":"Guangxi",
+    "1":100,
+    "2":111,
+    "3":127,
+    "4":139,
+    "5":150,
+    "6":168,
+    "7":172,
+    "8":183
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 114.9,37.89 ]
+    },
+    "properties": {
+    "FID":17,
+    "Providence":"Hebei",
+    "1":96,
+    "2":104,
+    "3":113,
+    "4":126,
+    "5":135,
+    "6":157,
+    "7":171,
+    "8":195
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 101.34,24.47 ]
+    },
+    "properties": {
+    "FID":18,
+    "Providence":"Yunnan",
+    "1":91,
+    "2":99,
+    "3":109,
+    "4":117,
+    "5":122,
+    "6":128,
+    "7":135,
+    "8":138
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 128.73,47.12 ]
+    },
+    "properties": {
+    "FID":19,
+    "Providence":"Heilongjiang",
+    "1":80,
+    "2":95,
+    "3":118,
+    "4":155,
+    "5":190,
+    "6":227,
+    "7":277,
+    "8":282
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 122.52,41.94 ]
+    },
+    "properties": {
+    "FID":20,
+    "Providence":"Liaoning",
+    "1":60,
+    "2":64,
+    "3":70,
+    "4":74,
+    "5":81,
+    "6":89,
+    "7":94,
+    "8":99
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 109.94,19.56 ]
+    },
+    "properties": {
+    "FID":21,
+    "Providence":"Hainan",
+    "1":57,
+    "2":63,
+    "3":70,
+    "4":79,
+    "5":89,
+    "6":100,
+    "7":111,
+    "8":123
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 111.85,37.24 ]
+    },
+    "properties": {
+    "FID":22,
+    "Providence":"Shanxi",
+    "1":47,
+    "2":56,
+    "3":66,
+    "4":74,
+    "5":81,
+    "6":90,
+    "7":96,
+    "8":104
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 104.28,35.75 ]
+    },
+    "properties": {
+    "FID":23,
+    "Providence":"Gansu",
+    "1":35,
+    "2":45,
+    "3":51,
+    "4":56,
+    "5":57,
+    "6":62,
+    "7":70,
+    "8":71
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 117.36,39.34 ]
+    },
+    "properties": {
+    "FID":24,
+    "Providence":"Tianjin",
+    "1":34,
+    "2":40,
+    "3":49,
+    "4":63,
+    "5":67,
+    "6":70,
+    "7":79,
+    "8":81
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 107.29,26.84 ]
+    },
+    "properties": {
+    "FID":25,
+    "Providence":"Guizhou",
+    "1":29,
+    "2":38,
+    "3":46,
+    "4":56,
+    "5":64,
+    "6":69,
+    "7":77,
+    "8":89
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 106.15,37.19 ]
+    },
+    "properties": {
+    "FID":26,
+    "Providence":"Ningxia",
+    "1":26,
+    "2":28,
+    "3":31,
+    "4":34,
+    "5":34,
+    "6":40,
+    "7":43,
+    "8":45
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 115.05,43.37 ]
+    },
+    "properties": {
+    "FID":27,
+    "Providence":"Inner Mongolia",
+    "1":23,
+    "2":26,
+    "3":33,
+    "4":37,
+    "5":42,
+    "6":46,
+    "7":49,
+    "8":50
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 87.53,42.52 ]
+    },
+    "properties": {
+    "FID":28,
+    "Providence":"Xingjiang",
+    "1":18,
+    "2":23,
+    "3":24,
+    "4":29,
+    "5":32,
+    "6":36,
+    "7":39,
+    "8":42
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 126.54,43.83 ]
+    },
+    "properties": {
+    "FID":29,
+    "Providence":"Jilin",
+    "1":17,
+    "2":21,
+    "3":31,
+    "4":42,
+    "5":54,
+    "6":59,
+    "7":65,
+    "8":69
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 114.16,22.31 ]
+    },
+    "properties": {
+    "FID":30,
+    "Providence":"Hong Kong SAR",
+    "1":13,
+    "2":14,
+    "3":15,
+    "4":15,
+    "5":18,
+    "6":21,
+    "7":24,
+    "8":26
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 120.96,23.69 ]
+    },
+    "properties": {
+    "FID":31,
+    "Providence":"Taipei",
+    "1":10,
+    "2":10,
+    "3":10,
+    "4":10,
+    "5":11,
+    "6":11,
+    "7":16,
+    "8":16
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 96.4,35.74 ]
+    },
+    "properties": {
+    "FID":32,
+    "Providence":"Qinghai",
+    "1":8,
+    "2":9,
+    "3":13,
+    "4":15,
+    "5":17,
+    "6":18,
+    "7":18,
+    "8":18
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 113.54,22.19 ]
+    },
+    "properties": {
+    "FID":33,
+    "Providence":"Macao SAR",
+    "1":7,
+    "2":7,
+    "3":8,
+    "4":8,
+    "5":10,
+    "6":10,
+    "7":10,
+    "8":10
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 88.78,30.15 ]
+    },
+    "properties": {
+    "FID":34,
+    "Providence":"Xizang",
+    "1":1,
+    "2":1,
+    "3":1,
+    "4":1,
+    "5":1,
+    "6":1,
+    "7":1,
+    "8":1
+    }
+  }
+]
+}
