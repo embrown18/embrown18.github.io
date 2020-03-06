523658046103cee986a13ebd99971021a6b3c6f7
embrown18 <35782514+embrown18@users.noreply.github.com>
2020-03-06 15:54:41 -0600

    Create dataCovid.js

---
+++ Lab5/dataCovid.js
@@ -0,0 +1,24 @@
+var dataC =
+{
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 112.23,30.73 ]
+    },
+    "properties": {
+    "FID":1,
+    "Providence":"Hubei",
+    "1":7153,
+    "2":9074,
+    "3":11177,
+    "4":13522,
+    "5":16678,
+    "6":19665,
+    "7":22112,
+    "8":24953
+    }
+  }]
+}
